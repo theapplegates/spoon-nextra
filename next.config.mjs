eca1fc14b5a0ecf19ae8cfd2e7b57019d6a9d2d2
Paul Applegate <paul@theapplegates.com>
2022-12-24 10:44:07 -0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.jsx',
+  staticImage: true,
+  defaultShowCopyCode: true,
+  readingTime: true
+})
+
+export default withNextra({
+  reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['nextra.paulapplegate.com']
+  },
+})
